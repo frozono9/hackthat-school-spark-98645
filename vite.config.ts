23ad8e712ce22155fa9ab7fd2a2a15c03a4884e2
LatorreSony <alex.latorre@sony.com>
2025-09-30 19:53:36 +0200

    Deployment hopefully?

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hackthat-school-spark-98645/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
